888b4b42b85cf417a7e032fd98e44575d3d59313
RamBhaktBajrangbali <106311618+RamBhaktBajrangbali@users.noreply.github.com>
2022-05-27 16:39:33 +0530

    Update authentication.js

--- authentication.js
+++ authentication.js
@@ -1,67 +1,57 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js";
-import { getAuth ,GoogleAuthProvider, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-auth.js";
-import {exists, getvideos, db} from "./database.js"
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-auth.js";
+import { exists, getvideos, db } from "./database.js";
 
 const config = {
-  apiKey: "AIzaSyCdzbcpM_Oe3NQgBmpJcJJq1cs0z5VFWYc",
+    apiKey: "AIzaSyCdzbcpM_Oe3NQgBmpJcJJq1cs0z5VFWYc",
     authDomain: "fir-web-login-906b6.firebaseapp.com",
     projectId: "fir-web-login-906b6",
     storageBucket: "fir-web-login-906b6.appspot.com",
     messagingSenderId: "1014146452701",
     appId: "1:1014146452701:web:39150ab67e871555db1f34",
-    measurementId: "G-X24EJNJLQV"
+    measurementId: "G-X24EJNJLQV",
 };
 
 const app = initializeApp(config);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider(app);
 
-getAuth().onAuthStateChanged(function(user) {
-	if (user) {
-
+getAuth().onAuthStateChanged(function (user) {
+    if (user) {
         document.getElementById("login_div").style.display = "block";
-        
+
         (async () => {
-
             const find = await exists(user.email);
-            if(find === false){
-                swal(
-                    "Authenticaion Failure",
-                    "Your account is not verified, \n please contact server owner to get your account verified.",
-                    "error"
-                )
-                signOut(auth).then(() => {}).catch((error) => {});
+            if (find === false) {
+                swal("Authenticaion Failure", "Your account is not verified, \n please contact server owner to get your account verified.", "warning.png");
+                signOut(auth)
+                    .then(() => {})
+                    .catch((error) => {});
                 return;
-            }
-
-            else{
-                swal(
-                    "Successfully Logged In",
-                    `Welcome ${user.displayName} !`,
-                    "success"
-                )
+            } else {
+                swal("Successfully Logged In", `Welcome ${user.displayName} !`, "success.png");
                 document.getElementById("navbar_1").style.display = "block";
                 document.getElementById("user_div").style.display = "block";
                 document.getElementById("login_div").style.display = "none";
                 getvideos(db);
             }
-
-        })()
-
-  	} 
-    else {
+        })();
+    } else {
         // No user is signed in.
-        document.getElementById("navbar_1").style.display = "none"
+        document.getElementById("navbar_1").style.display = "none";
         document.getElementById("user_div").style.display = "none";
         document.getElementById("login_div").style.display = "block";
     }
 });
 
-export function LOGIN(){
-    signInWithPopup(auth, provider).then((result) => {}).catch((error) => {});
-};
+export function LOGIN() {
+    signInWithPopup(auth, provider)
+        .then((result) => {})
+        .catch((error) => {});
+}
 
-
-export function LOGOUT(){
-    signOut(auth).then(() => {}).catch((error) => {});
+export function LOGOUT() {
+    signOut(auth)
+        .then(() => {})
+        .catch((error) => {});
 }
